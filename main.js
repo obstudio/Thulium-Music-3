e5b8b164a19d1abb651a3e216a9e24a24624ce08
Shigma <1700011071@pku.edu.cn>
2018-05-24 22:26:05 +0800

    add logo and tray

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const {app, Menu, Tray, BrowserWindow} = require('electron')
 
 const path = require('path')
 const url = require('url')
@@ -10,10 +6,12 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let loadingWindow
+let loaded = false
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, frame: false})
+  mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -37,7 +35,23 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  tray = new Tray('./assets/logo.ico')
+  tray.setToolTip('Thulium Music')
+  tray.setContextMenu(Menu.buildFromTemplate([
+    {
+      label: '退出',
+      role: 'quit'
+    },
+    {
+      label: '运行',
+      click: (menuItem, browserWindow, event) => {
+        createWindow()
+      }
+    }
+  ]))
+  createWindow()
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -56,5 +70,4 @@ app.on('activate', function () {
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
