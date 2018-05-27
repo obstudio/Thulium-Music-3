b232431f22f14b3223087c4b08699d2c8228c110
jjyyxx <obfishstudio@gmail.com>
2018-05-27 10:26:14 +0800

    migration of thulium music frontend seems different from pure webpages need more discussion

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ function createMainWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'dist', 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
