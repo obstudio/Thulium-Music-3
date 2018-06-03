074b4f2cfdc574be12d31d4ea23c369e9afd16dd
Shigma <1700011071@pku.edu.cn>
2018-06-03 20:38:42 +0800

    add user settings

--- user.js
+++ user.js
@@ -1,26 +1,50 @@
 const fs = require('fs')
 const path = require('path')
 const process = require('process')
+const defaultSettings = require('./default.json')
 
 class TmUser {
+  constructor() {
+    if (!fs.existsSync(TmUser.UserPath + 'settings.json')) {
+      if (!fs.existsSync(TmUser.UserPath)) fs.mkdirSync(TmUser.UserPath)
+      this.Settings = defaultSettings
+      this.saveSettings()
+    } else {
+      this.Settings = require(TmUser.UserPath + 'settings.json')
+    }
+  }
+
   saveSettings() {
-    fs.writeFile(TmUser.userPath + 'settings.json', JSON.stringify(this.settings), {
-      encoding: 'utf8'
-    })
+    fs.writeFileSync(
+      TmUser.UserPath + 'settings.json',
+      JSON.stringify(this.Settings),
+      { encoding: 'utf8' }
+    )
+  }
+
+  getSetting(key) {
+    if (this.Settings[key] === undefined) {
+      this.Settings[key] = defaultSettings[key]
+    }
+    return this.Settings[key]
+  }
+
+  setSetting(key, value) {
+    this.Settings[key] = value
+    this.saveSettings()
   }
 }
 
 switch (process.platform) {
 case 'win32': 
-  TmUser.UserPath = path.join(process.env.LOCALAPPDATA, 'Obstudio/Thulium/')
-  TmUser.DataPath = path.join(process.env.ALLUSERSPROFILE, 'Obstudio/Thulium/')
+  TmUser.UserPath = path.join(process.env.LOCALAPPDATA, 'Obstudio/Thulium 3/')
+  TmUser.DataPath = path.join(process.env.ALLUSERSPROFILE, 'Obstudio/Thulium 3/')
   break
 default:
   // DO SOMETHING
 }
 
-TmUser.Settings = require(TmUser.UserPath + 'settings.json')
 TmUser.LangList = require('./languages/index.json')
 
-module.exports = TmUser
+module.exports = new TmUser()
 
