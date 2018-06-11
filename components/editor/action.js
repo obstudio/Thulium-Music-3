ed0ea036dbfb2722b5484dc2514b4be5573b8c44
jjyyxx <obfishstudio@gmail.com>
2018-06-11 16:10:41 +0800

    remove redundant save action

--- components/editor/action.js
+++ components/editor/action.js
@@ -3,39 +3,6 @@ const { dialog } = require('electron').remote
 
 module.exports = [
   {
-    id: 'tm-save',
-    label: 'Save File',
-    keybindings: [ window.monaco.KeyMod.CtrlCmd | window.monaco.KeyCode.KEY_S ],
-    precondition: null,
-    keybindingContext: null,
-    contextMenuGroupId: 'navigation',
-    contextMenuOrder: 1.5,
-    run(editor) {
-      const tab = editor.vue.current
-      if (!tab.changed) return
-      if (tab.path) {
-        tab.save()
-        return
-      }
-      const firstLine = editor.getModel().getLineContent(1)
-      let name
-      if (firstLine !== '' && firstLine.startsWith('//')) {
-        name = firstLine.slice(2).trim()
-      } else {
-        name = editor.vue.$t('editor.new-file')
-      }
-      dialog.showSaveDialog(null, {
-        title: editor.vue.$t('editor.save-as'),
-        defaultPath: name,
-        filters: [
-          { name: editor.vue.$t('editor.thulium'), extensions: ['tm', 'tml'] },
-          { name: editor.vue.$t('editor.all-files'), extensions: ['*'] }
-        ]
-      }, path => tab.save(path))
-    }
-  },
-
-  {
     id: 'tm-play',
     label: 'Play/Pause',
     keybindings: [ window.monaco.KeyMod.CtrlCmd | window.monaco.KeyCode.KEY_P],
@@ -59,7 +26,6 @@ module.exports = [
       }
     }
   },
-  
   {
     id: 'tm-stop',
     label: 'Stop Playing',
