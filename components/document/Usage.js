f9efdd8e30c6cb81e209f7584e36554a2bb5b9ea
jjyyxx <obfishstudio@gmail.com>
2018-06-22 16:30:18 +0800

    line break

--- components/document/Usage.js
+++ components/document/Usage.js
@@ -8,7 +8,10 @@ module.exports = {
   },
   render: VueCompile(`<div class="usage">
     <blockquote v-for="(usage, index) in node.content" :key="index">
-      <component v-for="(comp, index) in usage" :is="comp.type" :node="comp" :key="index"/>
+      <template v-for="(comp, index) in usage">
+        <component :is="comp.type" :node="comp" :key="index"/>
+        <br>
+      </template>
     </blockquote>
   </div>`)
 }
